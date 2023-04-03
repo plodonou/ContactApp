2df554f4d6dd92fac8bd8d31bf0c23251aff69eb
Prudo Lodonou <prudolodonou.pl@gmail.com>
2023-04-03 10:28:07 -0500

    Implementation save() inside contact repository

--- src/main/java/contact/ContactApp/data/ContactRepository.java
+++ src/main/java/contact/ContactApp/data/ContactRepository.java
@@ -1,4 +1,19 @@
 package contact.ContactApp.data;
 
+import contact.ContactApp.domain.Contact;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class ContactRepository {
+
+    private Map<String, Contact> contacts = new HashMap<String, Contact>();
+
+    public void save(Contact contact){
+        contacts.put(contact.getFirstName(), contact);
+    }
+
+
 }
