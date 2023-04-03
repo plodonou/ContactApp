318d3f043138cb20fdbb5b1275b23a424df55c54
Prudo Lodonou <prudolodonou.pl@gmail.com>
2023-04-03 10:09:53 -0500

    Implementation Contact Domain

--- src/main/java/contact/ContactApp/domain/Contact.java
+++ src/main/java/contact/ContactApp/domain/Contact.java
@@ -1,4 +1,25 @@
 package contact.ContactApp.domain;
 
 public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public String getFirstName(){ return this.firstName; }
+    public void setFirstName(String firstName){ this.firstName = firstName; }
+
+    public String getLastName(){ return this.lastName; }
+    public void setLastName(){ this.lastName = lastName; }
+
+    public String getEmail(){ return this.email; }
+    public void setEmail(String email){ this.email = email; }
+
+    public String getPhone(){ return this.phone; }
+    public void setPhone(String phone){ this.phone = phone; }
+
+
+
+
 }
