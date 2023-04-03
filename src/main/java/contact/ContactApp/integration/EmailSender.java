091223fecab70833cf297a7a3083f5c9a885b363
Prudo Lodonou <prudolodonou.pl@gmail.com>
2023-04-03 11:25:41 -0500

    implementation of sendEmail() inside integration layer

--- src/main/java/contact/ContactApp/integration/EmailSender.java
+++ src/main/java/contact/ContactApp/integration/EmailSender.java
@@ -1,4 +1,10 @@
 package contact.ContactApp.integration;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailSender {
+    public void sendEmail(String message, String emailAddress){
+        System.out.println("Send email message " + message + "to " + emailAddress);
+    }
 }
