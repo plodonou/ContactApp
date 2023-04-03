a5fb8db06837e3768341585d68e83cf51b2fd00f
Prudo Lodonou <prudolodonou.pl@gmail.com>
2023-04-03 11:40:44 -0500

    addContact() implementation inside contactService Layer

--- src/main/java/contact/ContactApp/service/ContactService.java
+++ src/main/java/contact/ContactApp/service/ContactService.java
@@ -1,4 +1,24 @@
 package contact.ContactApp.service;
 
+import contact.ContactApp.data.ContactRepository;
+import contact.ContactApp.domain.Contact;
+import contact.ContactApp.integration.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContactService {
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    EmailSender emailSender;
+
+    public void addContact(Contact contact){
+        contactRepository.save(contact);
+        emailSender.sendEmail("Welcome", contact.getEmail());
+    }
+
+
 }
