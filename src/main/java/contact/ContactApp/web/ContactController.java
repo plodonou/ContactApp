133452d807af35c712882dd549f1448502ea8d1d
Prudo Lodonou <prudolodonou.pl@gmail.com>
2023-04-04 01:23:08 -0500

    getContact() implementation inside web layer

--- src/main/java/contact/ContactApp/web/ContactController.java
+++ src/main/java/contact/ContactApp/web/ContactController.java
@@ -1,4 +1,29 @@
 package contact.ContactApp.web;
 
+import contact.ContactApp.domain.Contact;
+import contact.ContactApp.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContactController {
+    @Autowired
+    ContactService contactService;
+
+    @GetMapping("contacts/{firstName}")
+    public ResponseEntity<?> getContact(@PathVariable String firstName){
+        Contact contact = contactService.findByName(firstName);
+        if (contact == null){
+            return new ResponseEntity<CustomErrorType>(new CustomErrorType("Contact with firstName = "+ firstName + "is not available"),
+            HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Contact>(contact, HttpStatus.OK);
+
+    }
+
 }
--- src/main/java/contact/ContactApp/web/CustomErrorType.java
+++ src/main/java/contact/ContactApp/web/CustomErrorType.java
@@ -2,4 +2,8 @@ package contact.ContactApp.web;
 
 public class CustomErrorType {
 
+    private String errorMessage;
+    public CustomErrorType(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
 }
